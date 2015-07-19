5634e873d33bde5fae5e8c4cfd800b5c2ffb2b94
Tapan Shah <tapan.shah@mail.utoronto.ca>
2015-07-19 00:08:40 -0400

    adding C# block puzzle project to git

---
+++ BlockPuzzle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlockPuzzle
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {            
+            PuzzleInstance puzzle = new PuzzleInstance(Globals.initialConfiguration); //could also supply any arbitrary initial configuration
+            PuzzleSolver solver = new PuzzleSolver(puzzle);
+            var listOfMovesToSolve = solver.solve(); 
+            if (listOfMovesToSolve != null)
+            {
+                printSolution(listOfMovesToSolve);
+            }
+        }
+
+        static void printSolution(List<Tuple<int, string>> listOfMoves)
+        {
+            int i = 0;
+            for (i = 0; i < listOfMoves.Count; i++)
+            {
+                Console.WriteLine("Step {0}: Move {1} {2}", i+1, listOfMoves[i].Item1, listOfMoves[i].Item2);                
+            }
+        }
+    }
+}
---
+++ BlockPuzzle/PuzzleStateNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace BlockPuzzle
+{
+    //is an object to encapsulate a particular state of the puzzleInstance
+    //has a history of previous moves leading up to current configuration, current configuration of puzzle instance
+    //responds to: getPrevMoves(), getCurrentConfiguration(), 
+    class PuzzleStateNode : TreeNode{
+         private string lastMove;
+         private BlockPuzzleConfigList currentConfiguration;
+         private bool solvedState = false;
+
+         public PuzzleStateNode(string prevMove, BlockPuzzleConfigList configuration)
+         {
+            lastMove = prevMove;
+            currentConfiguration = configuration;
+         }
+
+         public string getLastMove()
+         {
+             return lastMove;
+         }
+
+         public BlockPuzzleConfigList getCurrentConfiguration()
+         {
+             return currentConfiguration;
+         }    
+        
+        public bool SolvedState
+        {
+            get
+            {
+                return solvedState;
+            }
+            set
+            {
+                solvedState = value;
+            }
+        }    
+    }
+}
