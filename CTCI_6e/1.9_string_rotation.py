d9a06d5e71717530001e3ab9148874dd4ee8e6c4
Tapan Shah <tapan.shah@mail.utoronto.ca>
2016-06-19 12:40:04 -0400

    ctci_6e 1.9: more efficient soln

--- CTCI_6e/1.9_string_rotation.py
+++ CTCI_6e/1.9_string_rotation.py
@@ -10,14 +10,22 @@ def isSubstring(s1, s2):
 def is_string_rotation(s1,s2):
 	''' 
 		Returns True if s2 is a rotation of s1 using only one call to isSubstring. False o/w
+		Time Complexity: let a be len of s1, b be len of s2, and let  b > a => O(b)
+		Space Complexity: O(1)
+	'''
+
+	return isSubstring(s1, s2 + s2)	
+
+def is_string_rotation_old(s1,s2):
+	''' 
+		Returns True if s2 is a rotation of s1 using only one call to isSubstring. False o/w
 		Time Complexity: let a be len of s1, b be len of s2, and let  b > a => O(b log b)
 		Space Complexity: O(1)
 	'''
 
 	s1_sorted = ''.join(sorted(s1))
 	s2_sorted = ''.join(sorted(s2))
-	return isSubstring(s1_sorted, s2_sorted)		
-
+	return isSubstring(s1_sorted, s2_sorted)
 
 class Test_String_Rotation(unittest.TestCase):
 
